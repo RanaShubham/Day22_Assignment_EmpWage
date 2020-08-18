67f912ec876d5583bbf5e983fdc8b718e29f3f6d
Shubham Rana <ranashubham7296@gmail.com>
2020-08-18 16:18:48 +0530

    Calculating the sal of employee until either they have worked 100 hrs or have worked for 20 days

--- EmpWage.java
+++ EmpWage.java
@@ -1,32 +1,73 @@
 
 public class EmpWage {
 
+	
+	static int Wage_Per_Hour = 20;
+	static int Full_Time_Hours = 8;
+	static int Part_Time_Hours=4;
+	static int Working_Days_Per_Month=20;
+	static int Max_Wrk_Hrs=100;
+	static int dailyFullTimeWage=Wage_Per_Hour*Full_Time_Hours;
+	static int dailyPartTimeWage=Wage_Per_Hour*Part_Time_Hours;
+
+	
 	public static void main(String[] args) 
 	{
-		int Wage_Per_Hour = 20;
-		int Full_Day_Hours = 8;
-		int Is_Full_Time=1;
-		int Part_Time_Hours=4;
-		int Working_Days_Per_Month=20;
-		
-		int dailyFullTimeWage=Wage_Per_Hour*Full_Day_Hours;
-		int dailyPartTimeWage=Wage_Per_Hour*Part_Time_Hours;
-		
-		int empCheck = (int)(Math.floor(Math.random()*10)%2);
-		
-		switch (empCheck)
-		{
-		case 1: System.out.println("Employee is present");
-		System.out.println("Daily wage of full time employee is "+dailyFullTimeWage);
-		System.out.println("Monthly wage of full time employee is "+Working_Days_Per_Month*dailyFullTimeWage);
-		System.out.println("Daily wage of part time employee is "+dailyPartTimeWage);
-		System.out.println("Monthly wages of part time employee is "+Working_Days_Per_Month*dailyPartTimeWage);
-		break;
-		case 0: System.out.println("Employee is absent");
-		System.out.println("Enmployee is not present");
-		System.out.println("Daily wage of full time employee is 0"
-				+ "\nDaily wage of part time employee is 0");
-		}
+				
+		calcPartTimeWage();
+		calcFullTimeWage();
+				
+	}
+	
+	private static int getAttendance()
+	{
+		return (int)(Math.floor(Math.random()*10)%2);
 	}
 
+	
+	static void calcPartTimeWage()
+	{
+		int workHrsPartTime=0;
+		int daysOfMonth=0;
+		
+		while(true)
+		{
+			++daysOfMonth;
+			
+			if(getAttendance() == 1)
+			{
+				workHrsPartTime=workHrsPartTime+Part_Time_Hours;
+				
+				if (workHrsPartTime+Part_Time_Hours > Max_Wrk_Hrs || daysOfMonth > Working_Days_Per_Month)
+				{
+					break;
+				}
+			}
+		}
+		System.out.println("This month part time employee worked " +workHrsPartTime+" hours and earned "+workHrsPartTime*Wage_Per_Hour+" Rupees" );
+	}
+	
+	static void calcFullTimeWage()
+	{
+
+		int workHrsFullTime=0;
+		int daysOfMonth=0;
+		
+		while(true)
+		{
+			++daysOfMonth;
+			
+			if(getAttendance() == 1)
+			{
+				workHrsFullTime=workHrsFullTime+Full_Time_Hours;
+				
+				if (workHrsFullTime+Full_Time_Hours > Max_Wrk_Hrs || daysOfMonth > Working_Days_Per_Month)
+				{
+					break;
+				}
+			}
+		}
+		System.out.println("This month full time employee worked " +workHrsFullTime+" hours and earned "+workHrsFullTime*Wage_Per_Hour+" Rupees" );
+	
+	}
 }
